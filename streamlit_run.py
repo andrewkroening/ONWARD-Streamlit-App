dd2e6832a98e231ed79d5b5c5676274e35b16c11
Andrew Kroening <111026320+andrewkroening@users.noreply.github.com>
2022-10-18 19:53:38 -0400

    final updates

--- streamlit_run.py
+++ streamlit_run.py
@@ -36,9 +36,9 @@ st.title("Welcome to ONWARD")
 
 st.subheader("The OpenSky-National Weather and Aircraft Reporting Dashboard")
 
-col1, col2 = st.columns((1, 2))
+col1, col2 = st.columns((1, 3))
 
-col3, col4 = st.columns((1, 2))
+col3, col4 = st.columns((1, 3))
 
 with col1:
     st.metric(
@@ -50,12 +50,16 @@ with col1:
 
 with col2:
     if opensky_count > 5400:
-        st.warning(
+        st.error(
             "There are currently more than 5400 aircraft in the air. This is above some upper threshold the FAA thinks makes for a busy sky."
         )
-    if opensky_count < 5400:
+    elif opensky_count > 5000:
+        st.warning(
+            "There are currently more than 5000 aircraft in the air. This is getting crowded."
+        )
+    else:
         st.success(
-            "There are currently less than 5400 aircraft in the air. This is below some upper threshold the FAA thinks makes for a busy sky."
+            "There are currently less than 5000 aircraft in the air. The skies don't look too busy."
         )
 
 
@@ -68,7 +72,11 @@ with col3:
     )
 
 with col4:
-    if state_df["total_alerts"].sum() > 500:
+    if state_df["total_alerts"].sum() > 600:
+        st.error(
+            "There are currently more than 500 weather alerts nationwide. This seems like a lot, you might want to check local weather."
+        )
+    elif state_df["total_alerts"].sum() > 500:
         st.warning(
             "There are currently more than 500 weather alerts nationwide. This seems like a lot, you might want to check local weather."
         )
@@ -77,14 +85,18 @@ with col4:
             "There are currently less than 500 weather alerts nationwide. This seems like a good day to fly."
         )
 
+st.write("")
+
+st.subheader("Current Weather Alerts by State")
 
 st.caption(
     "The map below will display a state's name, the total active weather alerts and a severity score. The severity score is a weighted calculation for the severity of the weather alerts active in that state and is on a scale of 0 to 1."
 )
 
+st.write("")
 
 # create a map of the US
-m = folium.Map(location=[43, -100], zoom_start=4)
+m = folium.Map(location=[39, -98], zoom_start=4)
 # create a choropleth map
 cp = folium.Choropleth(
     geo_data=state_geo_json,
